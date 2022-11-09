ec8140a2a14419f8122241b9750f5b53c15a7bc0
Lev Logvinov <llogvinov356@gmail.com>
2022-11-10 01:33:20 +0300

    refactor tongue class - separated in small classes

--- Assets/Scripts/Player/Tongue.cs
+++ Assets/Scripts/Player/Tongue.cs
@@ -1,80 +1,61 @@
 ﻿using System;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace Player
 {
     public class Tongue : MonoBehaviour
     {
+        [SerializeField] private TongueHead _tongueHead;
         [SerializeField] private TongueBase _tongueBase;
-        [SerializeField] private Transform _tongueHead;
+        [Space] 
         [SerializeField] private float _hitSpeed;
-        
+
         private PlayerInput _playerInput;
-        private Queue<Vector3> _movePositions;
-        private Vector3 _originalPosition;
-        private Vector3 _nextPosition;
+
         private bool _isHitSet;
 
         private void Awake()
         {
             _playerInput = GetComponent<PlayerInput>();
-
-            _originalPosition = _tongueHead.position;
+        }
+        
+        private void OnEnable()
+        {
+            _playerInput.HitSetEvent += OnHitSet;
+            _tongueHead.HitEnded += OnHitEnded;
         }
 
-        private void OnEnable() => _playerInput.HitSetEvent += OnHitSet;
-
-        private void OnDisable() => _playerInput.HitSetEvent -= OnHitSet;
+        private void OnDisable()
+        {
+            _playerInput.HitSetEvent -= OnHitSet;
+            _tongueHead.HitEnded -= OnHitEnded;
+        }
 
         private void Update()
         {
-            if (_isHitSet)
-            {
-                Move(_tongueHead, _hitSpeed, Time.deltaTime);
-            }
-        }
-        
-        private void Move(Transform objectToMove, float speed, float t)
-        {
-            objectToMove.position = Vector3.MoveTowards(objectToMove.position, _nextPosition, speed * t);
-            CheckSnappingToPosition(objectToMove);
-            _tongueBase.ScaleTongue(_originalPosition, _tongueHead.position);
+            if (!_isHitSet) return;
+
+            _tongueHead.Move(_hitSpeed, Time.deltaTime);
+            _tongueBase.Stretch(_tongueHead.OriginalPosition, _tongueHead.transform.position);
         }
 
         private void OnHitSet(Vector3 hitPosition)
         {
             hitPosition.z = 0f;
             
-            _movePositions = new Queue<Vector3>();
-            _movePositions.Enqueue(hitPosition);
-            _movePositions.Enqueue(_originalPosition);
-
-            _nextPosition = _movePositions.Dequeue();
+            _tongueHead.SetPath(hitPosition);
+            _tongueHead.Visual.enabled = true;
+            _tongueBase.Visual.enabled = true;
             
             _isHitSet = true;
         }
 
-        private void CheckSnappingToPosition(Transform objectToCheck)
+        private void OnHitEnded()
         {
-            if (!((_nextPosition - objectToCheck.position).sqrMagnitude < 0.001f)) return;
+            _tongueHead.Visual.enabled = false;
+            _tongueBase.Visual.enabled = false;
             
-            SnapToPosition(objectToCheck, _nextPosition);
-                
-            if (_movePositions.Count > 0)
-            {
-                _nextPosition = _movePositions.Dequeue();
-            }
-            else
-            {
-                _isHitSet = false;
-            }
+            _isHitSet = false;
         }
-        
-        private void SnapToPosition(Transform objectToSnap, Vector3 position)
-        {
-            objectToSnap.position = position;
-        }
-        
     }
 }
--- Assets/Scripts/Player/TongueBase.cs
+++ Assets/Scripts/Player/TongueBase.cs
@@ -5,31 +5,28 @@ namespace Player
 {
     public class TongueBase : MonoBehaviour
     {
-        [Tooltip("Set Sprite Renderer if not on the same Game Object")]
-        [SerializeField] private SpriteRenderer _baseVisual;
+        private SpriteRenderer _baseVisual;
 
-        private Transform _tongueBase;
-
-        public SpriteRenderer BaseVisual => _baseVisual;
+        public SpriteRenderer Visual => _baseVisual;
 
         private void Awake()
         {
-            if (_baseVisual == null) _baseVisual = GetComponent<SpriteRenderer>();
-            _tongueBase = _baseVisual.transform;
+            _baseVisual = GetComponent<SpriteRenderer>();
+            _baseVisual.enabled = false;
         }
 
-        public void ScaleTongue(Vector3 startPosition, Vector3 endPosition)
+        public void Stretch(Vector3 startPosition, Vector3 endPosition)
         {
             Vector3 centerPosition = (endPosition + startPosition) / 2f;
-            _tongueBase.position = centerPosition;
+            transform.position = centerPosition;
 
             Vector3 direction = (endPosition - startPosition).normalized;
-            _tongueBase.up = direction;
+            transform.up = direction;
 
             Vector3 scale = new Vector3(0.15f, 1f, 1f);
             scale.y = Vector3.Distance(startPosition, endPosition);
             scale.y *= 0.6f;
-            _tongueBase.localScale = scale;
+            transform.localScale = scale;
         }
     }
 }
