d691b17fee8df9e835edce23a06202b4925ad9ae
Lev Logvinov <llogvinov356@gmail.com>
2022-11-20 21:16:11 +0300

    add menu scene and scene management

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace FrogGame
+{
+    public class SceneController : MonoBehaviour
+    {
+        public void ReloadScene() =>
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
+        public void LoadMenuScene() => 
+            SceneManager.LoadScene(0);
+    }
+}
