9fd46d1a9ea9067125ac37ecf123969eebccc2d4
Lev Logvinov <llogvinov356@gmail.com>
2022-11-26 20:39:42 +0300

    add base class and implement logic for tongue head

---
+++ Assets/Scripts/testMover/BasePathMover.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FrogGame
+{
+    public class BasePathMover: MonoBehaviour
+    {
+        public Action MoveEnded; 
+        
+        [SerializeField] private float _moveSpeed;
+        
+        protected Queue<Vector3> MovePositions { get; set; }
+        protected Vector3 OriginalPosition;
+        private Vector3 _nextPosition;
+
+        private Coroutine _moveCoroutine;
+
+        private void Awake()
+        {
+            OriginalPosition = transform.position;
+        }
+
+        protected void MoveToNextPosition()
+        {
+            _nextPosition = MovePositions.Dequeue();
+            _moveCoroutine = StartCoroutine(MoveTransform());
+        }
+        
+        protected IEnumerator MoveTransform()
+        {
+            while (!((_nextPosition - transform.position).sqrMagnitude < 0.001f))
+            {
+                transform.position = Vector3.MoveTowards(
+                    transform.position, 
+                    _nextPosition, 
+                    _moveSpeed * Time.deltaTime);
+
+                yield return null;
+            }
+            
+            SnapToPosition(transform, _nextPosition);
+            if (MovePositions.Count > 0)
+            {
+                MoveToNextPosition();
+            }
+            else
+            {
+                MoveEnded?.Invoke();
+            }
+        }
+
+        private void SnapToPosition(Transform transformToSnap, Vector3 position)
+        {
+            transformToSnap.position = position;
+        }
+
+        public void ForceEndMoving()
+        {
+            StopCoroutine(_moveCoroutine);
+        }
+        
+        public void ForceMoveToOriginalPosition()
+        {
+            if (_nextPosition == OriginalPosition) return;
+
+            _nextPosition = MovePositions.Dequeue();
+        }
+    }
+}
---
+++ Assets/Scripts/testMover/TongueHeadV2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FrogGame
+{
+    public class TongueHeadV2 : BasePathMover
+    {
+        private Vector3 _hitPosition;
+        
+        private PlayerInput _playerInput;
+
+        private void Start()
+        {
+            _playerInput = GetComponentInParent<PlayerInput>();
+            _playerInput.HitSetEvent += OnHitSet;
+        }
+
+        private void OnDestroy()
+        {
+            _playerInput.HitSetEvent += OnHitSet;
+        }
+
+        private void OnHitSet(Vector3 hitPosition)
+        {
+            _hitPosition = hitPosition;
+            _hitPosition.z = 0f;
+
+            MovePositions = new Queue<Vector3>();
+            MovePositions.Enqueue(_hitPosition);
+            MovePositions.Enqueue(OriginalPosition);
+            
+            MoveToNextPosition();
+        }
+    }
+}
