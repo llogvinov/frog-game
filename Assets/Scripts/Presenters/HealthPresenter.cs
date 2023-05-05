2b6c120e786a762efc96963a242a6f004320a0e6
Lev Logvinov <llogvinov356@gmail.com>
2023-05-05 22:28:32 +0300

    Configured health presenter

--- Assets/Scripts/Presenters/HealthPresenter.cs
+++ Assets/Scripts/Presenters/HealthPresenter.cs
@@ -1,4 +1,6 @@
-﻿using Player;
+﻿using System;
+using Player;
+using Settings;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,6 +10,24 @@ namespace Presenters
     {
         [SerializeField] private Canvas _healthCanvas;
         [SerializeField] private LayoutGroup _layoutGroup;
+        [SerializeField] private HealthSettings _healthSettings;
+        [Space]
+        [SerializeField] private Image _iconPrefab;
+        [SerializeField] private Sprite _fullHeart;
+        [SerializeField] private Sprite _emptyHeart;
+
+        private Image[] _icons;
+
+        private void Awake()
+        {
+            _icons = new Image[_healthSettings.MaxHealth];
+            
+            for (int i = 0; i < _layoutGroup.transform.childCount; i++) 
+                Destroy(_layoutGroup.transform.GetChild(i).gameObject);
+
+            for (int i = 0; i < _icons.Length; i++) 
+                _icons[i] = Instantiate(_iconPrefab, _layoutGroup.transform);
+        }
 
         private void Start()
         {
@@ -29,7 +49,10 @@ namespace Presenters
         private void UpdateHealthUI(Health health)
         {
             var healthValue = health.CurrentHealth;
-            
+            for (int i = 0; i < _icons.Length; i++)
+            {
+                _icons[i].sprite = healthValue > i ? _fullHeart : _emptyHeart;
+            }
         }
     }
 }
