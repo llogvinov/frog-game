b0d433f35f239384772251cc9bc2b9c626ffd329
Lev Logvinov <llogvinov356@gmail.com>
2023-05-05 20:21:38 +0300

    Refactored Health script

--- Assets/Scripts/Player/Health.cs
+++ Assets/Scripts/Player/Health.cs
@@ -1,40 +1,42 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class Health
+namespace Player
 {
-    private readonly int _minHealth;
-    private readonly int _maxHealth;
+    public class Health
+    {
+        private readonly int _minHealth;
+        private readonly int _maxHealth;
     
-    private int _currentHealth;
+        private int _currentHealth;
 
-    public int CurrentHealth => _currentHealth;
-
-    public Health(int minHealth, int maxHealth)
-    {
-        _minHealth = minHealth;
-        _maxHealth = maxHealth;
-
-        _currentHealth = _maxHealth;
-    }
-
-    public void TakeDamage(int value)
-    {
-        _currentHealth = Mathf.Clamp(_currentHealth - value, _minHealth, _maxHealth);
-        
-        if (_currentHealth == _minHealth)
+        public Health(int minHealth, int maxHealth)
         {
-            GameManager.Instance.GameOver?.Invoke();
+            _minHealth = minHealth;
+            _maxHealth = maxHealth;
+
+            ResetHealth();
         }
-    }
 
-    public void Heal(int value)
-    {
-        _currentHealth = Mathf.Clamp(_currentHealth + value, _minHealth, _maxHealth);
-    }
+        public void TakeDamage(int value)
+        {
+            _currentHealth = Mathf.Clamp(_currentHealth - value, _minHealth, _maxHealth);
+            CheckHealth();
+        }
 
-    public void ResetHealth()
-    {
-        _currentHealth = _maxHealth;
+        public void Heal(int value)
+        {
+            _currentHealth = Mathf.Clamp(_currentHealth + value, _minHealth, _maxHealth);
+        }
+
+        public void ResetHealth()
+        {
+            _currentHealth = _maxHealth;
+        }
+
+        private void CheckHealth()
+        {
+            if (_currentHealth == _minHealth) 
+                GameManager.Instance.GameOver?.Invoke();
+        }
     }
 }
