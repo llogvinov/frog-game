d38eb8412ef374a5ef507511ac8c7d116685d606
Lev Logvinov <llogvinov356@gmail.com>
2023-05-05 22:01:33 +0300

    Added health settings SO

---
+++ Assets/Scripts/Settings/HealthSettings.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Settings
+{
+    [CreateAssetMenu(fileName = "HealthSettings", menuName = "ObjectSettings/HealthSettings")]
+    public class HealthSettings : ScriptableObject
+    {
+        [SerializeField] private int _minHealth;
+        [SerializeField] private int _maxHealth;
+
+        public int MinHealth => _minHealth;
+
+        public int MaxHealth => _maxHealth;
+    }
+}
