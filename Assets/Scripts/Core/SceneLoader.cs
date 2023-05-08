9221556a62fb12b5ed1b635c4b998ce13d165404
Lev Logvinov <llogvinov356@gmail.com>
2023-05-08 20:15:19 +0300

    Added SceneLoader

---
+++ Assets/Scripts/Core/SceneLoader.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Core
+{
+    public class SceneLoader
+    {
+        private readonly ICoroutineRunner _coroutineRunner;
+        
+        public SceneLoader(ICoroutineRunner coroutineRunner) 
+            => _coroutineRunner = coroutineRunner;
+
+        public void Load(string name, Action onLoaded = null)
+            => _coroutineRunner.StartCoroutine(LoadScene(name, onLoaded));
+        
+        private IEnumerator LoadScene(string name, Action onLoaded = null)
+        {
+            AsyncOperation waitLoadScene = SceneManager.LoadSceneAsync(name);
+
+            while (!waitLoadScene.isDone)
+                yield return null;
+            
+            onLoaded?.Invoke();
+        }
+    }
+}
